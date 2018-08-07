91b5cf38efea6d59da7cd56c46d6980f8626878c
Mumin Mian <m.mian0122@gmail.com>
2018-08-07 03:06:28 -0400

    Fix generate grid issue

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-
 // Global variables
 const container = document.querySelector(".container");
-// Request function
+// Generate grid items
+const generateGrid = function(data) {
+  let userData = data.results;
+  const userElements = userData.map((function(user) {
+    container.innerHTML +=
+      `<div class="galleryItem">
+        <div class="imageDiv">
+          <img src="${user.picture.medium}" alt="User Avatar">
+        </div>
+        <div class="info">
+          <h2>${user.name.first} ${user.name.last}</h2>
+          <p>${user.email}</p>
+          <p>${user.location.city}</p>
+        </div>
+      </div>`
+  }));
+};
+// Fetch request
 fetch("https://randomuser.me/api/?inc=name,email,location,picture&results=12")
   .then(response => response.json())
   .then(data => generateGrid(data))
   .catch(response => new Error(response.statusText));
-// Generate grid items
-const generateGrid = function(data) {
-  let userData = data.results;
-  var userElements = userData.map((function(user) {
-    console.log(user);
-    `<div class="galleryItem">
-      <div class="imageDiv">
-        <img src="${user.picture.medium}" alt="User Avatar">
-      </div>
-      <div class="info">
-        <h2>${user.name}</h2>
-        <p>${user.email}</p>
-        <p>${user.location.city}</p>
-      </div>
-    </div>`
-  }));
-  container.innerHTML += userElements;
-};
