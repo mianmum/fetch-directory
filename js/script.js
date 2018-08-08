405533b7f3c6b96f39b408774ac598dbc3f3019b
Mumin Mian <m.mian0122@gmail.com>
2018-08-07 23:04:11 -0400

    Add modal window and CSS animations

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 // Global variables
 const container = document.querySelector(".container");
+const modal = document.querySelector('.modal');
+const modalBox = document.querySelector('.modalBox');
+let galleryItems = "";
+let userData = "";
+let closeButton = "";
+// Capitalize first letter
+const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
 // Generate grid items
 const generateGrid = function(data) {
-  let userData = data.results;
+  userData = data.results;
   const userElements = userData.map((function(user) {
     container.innerHTML +=
       `<div class="galleryItem">
@@ -10,15 +17,46 @@ const generateGrid = function(data) {
           <img src="${user.picture.medium}" alt="User Avatar">
         </div>
         <div class="info">
-          <h2>${user.name.first} ${user.name.last}</h2>
+          <h2>${capitalize(user.name.first)} ${capitalize(user.name.last)}</h2>
           <p>${user.email}</p>
-          <p>${user.location.city}</p>
+          <p>${capitalize(user.location.city)}</p>
         </div>
       </div>`
   }));
 };
+// Fill Modal
+const modalFill = function(user) {
+  galleryItems = Array.from(document.querySelectorAll('.galleryItem'));
+  console.log(galleryItems);
+  modalBox.innerHTML = "";
+  modalBox.innerHTML +=
+    user.innerHTML
+  + `
+    <hr>
+    <p>${userData[galleryItems.indexOf(user)].phone}</p>
+    <p>${userData[galleryItems.indexOf(user)].location.street}, ${userData[galleryItems.indexOf(user)].location.postcode}</p>
+    <p>Birthday: ${userData[galleryItems.indexOf(user)].dob.date.slice(0, 10)}</p>
+    <span class="close-button">&times;</span
+    `
+};
+// Modal close
+const closeModal = function() {
+  closeButton = document.querySelector('.close-button');
+  closeButton.addEventListener('click', () => modal.classList.toggle('show-modal'));
+};
+// Modal
+const modalClick = () => {
+  container.addEventListener('click', e => {
+    if (e.target.classList.contains('galleryItem')) {
+      modal.classList.toggle('show-modal');
+      modalFill(e.target);
+      closeModal();
+    };
+  });
+};
 // Fetch request
-fetch("https://randomuser.me/api/?inc=name,email,location,picture&results=12")
+fetch("https://randomuser.me/api/?inc=name,email,location,picture,phone,dob&nat=ca&results=12")
   .then(response => response.json())
   .then(data => generateGrid(data))
+  .then(modalClick())
   .catch(response => new Error(response.statusText));
